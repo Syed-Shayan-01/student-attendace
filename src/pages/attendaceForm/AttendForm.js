c91c9a62679ef9f6848a422a43a740293debf3e0
Syed Shayan <shayansyed712@gmail.com>
2024-01-02 00:06:29 +0500

    new changes

--- src/pages/attendaceForm/AttendForm.js
+++ src/pages/attendaceForm/AttendForm.js
@@ -1,45 +1,69 @@
-import React, { useState } from 'react';
-import { Modal, Upload } from 'antd';
+import React, { useRef, useState } from 'react';
 const AttendForm = () => {
+    const [Images, setImages] = useState(null);
+    const fileInputRef = useRef();
+    console.log(Images)
+    const handleImage = (e) => {
+        const form = e.target.files[0]
+        const imageUrl = URL.createObjectURL(form);
+        setImages(imageUrl);
+    }
+
+    const handleImageClick = () => {
+        // Trigger a click on the file input
+        fileInputRef.current.click();
+    };
+
     return (
         <>
-            <section class="text-gray-600 body-font relative">
-                <div class="container px-5 py-24 mx-auto">
-                
-                    <div class="lg:w-1/2 md:w-2/3 mx-auto">
-                        <div class="flex flex-wrap -m-2">
-                            <div class="p-2 w-full">
-                                <div class="relative">
-                                    <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-                                    <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <section className="text-gray-600 body-font relative">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                        <div className="flex justify-center  flex-wrap -m-2">
+                            <input
+                                className='absolute w-1 text-white'
+                                type="file"
+                                accept="image/*"
+                                onChange={handleImage}
+                                ref={fileInputRef}
+                            // value={Images}
+                            ></input>
+                            {Images ? <img src={`${Images}`} onClick={handleImageClick} className='w-40 h-40 z-[1] rounded-full' /> :
+                                <img src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+                                    onClick={handleImageClick}
+                                    className='w-40 z-[1] rounded-full ' />}
+                            <div className="p-2 w-full">
+                                <div className="relative">
+                                    <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
+                                    <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-1/2">
-                                <div class="relative">
-                                    <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
+                                    <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-1/2">
-                                <div class="relative">
-                                    <label for="password" class="leading-7 text-sm text-gray-600">Password</label>
-                                    <input type="password" id="password" name="password" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="password" className="leading-7 text-sm text-gray-600">Password</label>
+                                    <input type="password" id="password" name="password" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-1/2">
-                                <div class="relative">
-                                    <label for="CourseName" class="leading-7 text-sm text-gray-600">Course</label>
-                                    <input type="CourseName" id="CourseName" name="CourseName" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="CourseName" className="leading-7 text-sm text-gray-600">Course</label>
+                                    <input type="CourseName" id="CourseName" name="CourseName" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-1/2">
-                                <div class="relative">
-                                    <label for="Number" class="leading-7 text-sm text-gray-600">Phone Number</label>
-                                    <input type="Number" id="Number" name="Number" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <div className="p-2 w-1/2">
+                                <div className="relative">
+                                    <label htmlFor="Number" className="leading-7 text-sm text-gray-600">Phone Number</label>
+                                    <input type="Number" id="Number" name="Number" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
-                            <div class="p-2 w-full">
-                                <button class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
+                            <div className="p-2 w-full">
+                                <button className="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
                             </div>
                         </div>
                     </div>
