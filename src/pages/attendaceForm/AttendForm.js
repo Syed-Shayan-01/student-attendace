3466091211ccf89aa20c02fbab34dbf36c3966ac
Syed Shayan <shayansyed712@gmail.com>
2024-01-18 13:47:08 +0500

    new change

--- src/pages/attendaceForm/AttendForm.js
+++ src/pages/attendaceForm/AttendForm.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import baseUrl from '../../config/BaseUrl';
 import { useNavigate } from 'react-router-dom';
 const AttendForm = () => {
@@ -7,14 +7,14 @@ const AttendForm = () => {
         email: '',
         password: '',
         course: '',
-        phoneNumber: Boolean,
+        phoneNumber: '',
         image: null
     });
     // const fileInputRef = useRef();
     const navigate = useNavigate();
 
     const handleForm = (e) => {
-        console.log(setFormData({ ...formData, [e.target.name]: e.target.value }))
+        setFormData({ ...formData, [e.target.name]: e.target.value })
     }
     const handleImage = (e) => {
         setFormData({ ...formData, image: e.target.files[0] })
@@ -22,17 +22,17 @@ const AttendForm = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         // const data = { name, email, password, course, phoneNumber, image }
+
         const formDataSend = new FormData();
         formDataSend.append('name', formData.name)
         formDataSend.append('email', formData.email)
         formDataSend.append('password', formData.password)
         formDataSend.append('course', formData.course)
         formDataSend.append('phoneNumber', formData.phoneNumber)
-        formData.append('image', formData.image)
-
+        formDataSend.append('image', formData.image)
 
         try {
-            const response = await fetch(`${baseUrl}attendance/`, {
+            const response = await fetch(`${baseUrl}attendance`, {
                 method: "POST", // or 'PUT'
                 headers: {
                     "Content-Type": "application/json",
@@ -57,7 +57,7 @@ const AttendForm = () => {
                     <div className="lg:w-1/2 md:w-2/3 mx-auto">
                         <form onSubmit={handleSubmit} className="flex justify-center  flex-wrap -m-2">
                             <input
-                                className='absolute w-1 text-white'
+                                className='absolute'
                                 type="file"
                                 accept="image/*"
                                 onChange={handleImage}
@@ -94,8 +94,8 @@ const AttendForm = () => {
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
-                                    <label htmlFor="Number" className="leading-7 text-sm text-gray-600">Phone Number</label>
-                                    <input type="Number" id="Number" name="Number" value={formData.phoneNumber} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <label htmlFor="phoneNumber" className="leading-7 text-sm text-gray-600">Phone Number</label>
+                                    <input type="phoneNumber" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
