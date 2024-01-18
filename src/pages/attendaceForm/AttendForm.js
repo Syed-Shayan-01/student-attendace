0e516a249f5430eac3a23294ca0e1a80b0248ab6
Syed Shayan <shayansyed712@gmail.com>
2024-01-18 12:53:47 +0500

    short end method use input

--- src/pages/attendaceForm/AttendForm.js
+++ src/pages/attendaceForm/AttendForm.js
@@ -2,37 +2,42 @@ import React, { useRef, useState } from 'react';
 import baseUrl from '../../config/BaseUrl';
 import { useNavigate } from 'react-router-dom';
 const AttendForm = () => {
-    const [image, setImages] = useState(null);
-    const [name, setname] = useState('');
-    const [email, setemail] = useState('');
-    const [password, setpassword] = useState('');
-    const [course, setcourse] = useState('');
-    const [phoneNumber, setphoneNumber] = useState()
-    const fileInputRef = useRef();
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        course: '',
+        phoneNumber: Boolean,
+        image: null
+    });
+    // const fileInputRef = useRef();
     const navigate = useNavigate();
-    const handleImage = (e) => {
 
-        const form = e.target.files[0];
-        const Image = new FormData();
-        Image.append('image', form.image)
-        setImages(form)
+    const handleForm = (e) => {
+        console.log(setFormData({ ...formData, [e.target.name]: e.target.value }))
     }
-
-    const handleImageClick = () => {
-        // Trigger a click on the file input
-        fileInputRef.current.click();
-    };
-
+    const handleImage = (e) => {
+        setFormData({ ...formData, image: e.target.files[0] })
+    }
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const data = { name, email, password, course, phoneNumber, image }
+        // const data = { name, email, password, course, phoneNumber, image }
+        const formDataSend = new FormData();
+        formDataSend.append('name', formData.name)
+        formDataSend.append('email', formData.email)
+        formDataSend.append('password', formData.password)
+        formDataSend.append('course', formData.course)
+        formDataSend.append('phoneNumber', formData.phoneNumber)
+        formData.append('image', formData.image)
+
+
         try {
             const response = await fetch(`${baseUrl}attendance/`, {
                 method: "POST", // or 'PUT'
                 headers: {
                     "Content-Type": "application/json",
                 },
-                body: JSON.stringify(data),
+                body: JSON.stringify(formDataSend),
             });
 
 
@@ -56,42 +61,41 @@ const AttendForm = () => {
                                 type="file"
                                 accept="image/*"
                                 onChange={handleImage}
-                                ref={fileInputRef}
                             />
-                            {image ?
+                            {/* {image ?
                                 <img src={`${image}`} onClick={handleImageClick} className='w-40 h-40 z-[1] rounded-full' /> :
                                 <img src='/Images/profile.png'
                                     onClick={handleImageClick}
                                     className='w-40 z-[1] rounded-full ' />
-                            }
+                            } */}
                             <div className="p-2 w-full">
                                 <div className="relative">
                                     <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-                                    <input type="text" id="name" name="name" value={name} onChange={(e) => { setname(e.target.value) }} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="text" id="name" name="name" value={formData.name} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                                    <input type="email" id="email" name="email" value={email} onChange={(e) => { setemail(e.target.value) }} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="email" id="email" name="email" value={email} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="password" className="leading-7 text-sm text-gray-600">Password</label>
-                                    <input type="password" id="password" name="password" value={password} onChange={(e) => { setpassword(e.target.value) }} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="password" id="password" name="password" value={password} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="course" className="leading-7 text-sm text-gray-600">Course</label>
-                                    <input type="course" id="course" name="course" value={course} onChange={(e) => { setcourse(e.target.value) }} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="course" id="course" name="course" value={course} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-1/2">
                                 <div className="relative">
                                     <label htmlFor="Number" className="leading-7 text-sm text-gray-600">Phone Number</label>
-                                    <input type="Number" id="Number" name="Number" value={phoneNumber} onChange={(e) => { setphoneNumber(e.target.value) }} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                                    <input type="Number" id="Number" name="Number" value={phoneNumber} onChange={handleForm} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                                 </div>
                             </div>
                             <div className="p-2 w-full">
@@ -106,4 +110,4 @@ const AttendForm = () => {
     )
 }
 
-export default AttendForm
+export default AttendForm;
